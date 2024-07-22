6825abc6a75790900c0fc8cb32a149475641a668
Ajay <pajay7686@gmail.com>
2024-07-23 03:20:08 +0530

    Revert "Revert "Commit changes before removing .env from tracking""
    This reverts commit a69f5ec4ee7856dc8059c2403666ba90586c0c80.

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,30 +1,33 @@
 "use client";
-import React from "react";
+
 import Link from "next/link";
 import Image from "next/image";
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
 import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 
 const Nav = () => {
-  const isUserLoggedIn = true;
+  const { data: session } = useSession();
+  console.log("data,", session);
+
   const [providers, setProviders] = useState(null);
   const [toggleDropdown, setToggleDropdown] = useState(false);
   const handleToggle = () => {
     setToggleDropdown((prev) => !prev);
   };
   useEffect(() => {
-    //IIFE
     (async () => {
       const res = await getProviders();
+      console.log("prov", res);
       setProviders(res);
     })();
   }, []);
+
   return (
     <nav className="flex-between w-full mb-16 pt-3">
       <Link href="/" className="flex gap-2 flex-center">
         <Image
           src="/assets/images/logo.svg"
-          alt="Promtopia Logo"
+          alt="logo"
           width={30}
           height={30}
           className="object-contain"
@@ -34,26 +37,23 @@ const Nav = () => {
 
       {/* Desktop Navigation */}
       <div className="sm:flex hidden">
-        {isUserLoggedIn ? (
-          <div className="flex gap-3 md-gap-5">
+        {session?.user ? (
+          <div className="flex gap-3 md:gap-5">
             <Link href="/create-prompt" className="black_btn">
               Create Post
             </Link>
-            <button
-              className="outline_btn"
-              onClick={() => {
-                console.log("signing OUT");
-              }}
-            >
+
+            <button type="button" onClick={signOut} className="outline_btn">
               Sign Out
             </button>
+
             <Link href="/profile">
               <Image
-                src="/assets/images/logo.svg"
-                alt="Profile"
-                className="rounded-full"
+                src={session?.user.image}
                 width={37}
                 height={37}
+                className="rounded-full"
+                alt="profile"
               />
             </Link>
           </div>
@@ -75,12 +75,13 @@ const Nav = () => {
           </>
         )}
       </div>
+
       {/* Mobile Navigation */}
       <div className="sm:hidden flex relative">
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className="flex">
             <Image
-              src="/assets/images/logo.svg"
+              src={session?.user.image}
               width={37}
               height={37}
               className="rounded-full"
