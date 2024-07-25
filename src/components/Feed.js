e3365051941cbd434265b81a989f3d177a06f166
Ajay <pajay7686@gmail.com>
2024-07-26 04:13:37 +0530

    GET route created for prompts

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,5 +1,54 @@
+"use client";
+import { useEffect, useState } from "react";
+import PromptCard from "./PromptCard";
+
+const PromptCardList = ({ data, handleTagClick }) => {
+  return (
+    <div className="mt-16 prompt_layout">
+      {data.map((post) => {
+        <PromptCard
+          key={post._id}
+          post={post}
+          handleTagClick={handleTagClick}
+        />;
+      })}
+    </div>
+  );
+};
 const Feed = () => {
-  return <div>Feed</div>;
+  const [searchText, setsearchText] = useState("");
+  const [allPost, setAllPost] = useState([]);
+
+  const handleSearchChange = (e) => {};
+  const fetchPosts = async () => {
+    try {
+      const response = await fetch("/api/prompt");
+      const data = await response.json();
+      setAllPost(data);
+    } catch (error) {
+      console.log("Error at Retreiving post", err);
+    }
+  };
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  return (
+    <section className="feed">
+      <form className="relative w-full flex-center">
+        <input
+          type="text"
+          placeholder="Search for a tag or a username"
+          value={searchText}
+          onChange={handleSearchChange}
+          required
+          className="search_input peer"
+        />
+      </form>
+      <PromptCardList data={allPost} handleTagClick={() => {}} />
+    </section>
+  );
 };
 
 export default Feed;
--- src/components/PromptCard.js
+++ src/components/PromptCard.js
@@ -1,7 +1,17 @@
+"use client";
 import React from "react";
 
-const PromptCard = () => {
-  return <div>PromptCard</div>;
+const PromptCard = ({ post, handleTagClick }) => {
+  console.log("post", post);
+  return (
+    <div className="prompt_card">
+      <div className="flex justify-between items-start gap-5">
+        <div>Image</div>
+        <div>Username</div>
+        <div>EMail</div>
+      </div>
+    </div>
+  );
 };
 
 export default PromptCard;
